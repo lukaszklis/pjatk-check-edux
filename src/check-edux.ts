a8865913eafdec1142be224bf80004ca2e0576b3
Lukasz Klis <lukasz@klis.pl>
2018-03-25 15:37:22 +0200

    :zap: Refactor the tool to provide better UX

--- src/check-edux.ts
+++ src/check-edux.ts
@@ -2,25 +2,26 @@ import * as program from 'commander';
 import chalk from 'chalk';
 import { renderInfo } from './helper/messages-helper';
 import { hasCredentials } from './helper/store-helper';
-import { getEduxNotifications } from './step/check-edux';
+import { crawl } from './step/crawl';
 import { initialize } from './step/initialize';
 import { clear } from './step/clear';
 
 program
-    .option('-i, --init', 'Initialize `check-edux` on your computer')
     .option('-c, --clear', 'Clear `check-edux`’s storage on your computer')
     .parse(process.argv);
 
-if (program.init) {
-    renderInfo('Configuring check-edux…');
-    initialize();
-} else if (program.clear) {
-    renderInfo('Clearing check-edux’s store…');
-    clear();
-} else {
-    if (!hasCredentials()) {
-        throw new Error(chalk.red('Credentials are not set in the .env file!'));
-    }
+async function checkEdux(): Promise<void> {
+    if (program.clear) {
+        renderInfo('Clearing check-edux’s store…');
+        clear();
+    } else {
+        if (!hasCredentials()) {
+            renderInfo('Configuring check-edux…');
+            await initialize();
+        }
 
-    getEduxNotifications().catch(console.error.bind(console));
+        crawl().catch(console.error.bind(console));
+    }
 }
+
+checkEdux();
